90b0664e0c6590c997f3631681f437179e59ff15
Antonina <polonchuk.a@gmail.com>
2022-08-09 22:12:15 +0300

    <Домиков стало больше, ведут себя страньше>

--- JavaScript1.js
+++ JavaScript1.js
@@ -1,37 +1,255 @@
 ﻿var $drag = $('.move').draggabilly({})
 
-let house;
 
-//$(document).ready(function () {
-//    $("#Дом_2").hide();
-//    $("#Дом_3").hide();
-//    $("#Дом_4").hide();
-//    $("#Дом_5").hide();
-//    house = 1;
-//});
+var clickId;
+
+// house.Yl2a
+$('.house.Yl2a > #Дом_1').click(function () {
+
+    //console.log($(this).parent().attr("class"))
+    //clickId = $(this).parent().attr('class');
+    //console.log(clickId)
+
+    $('.house.Yl2a > #Дом_1').hide();
+    $('.house.Yl2a > #Дом_2').show();
 
 
-    $("#Дом_1").click(function () {
-        $("#Дом_1").hide();
-        $("#Дом_2").show();
+     //$(clickId > '#Дом_1').hide();
+     //$(clickId > '#Дом_2').show();
 
-    });
+    //if ($("house.Yl2a") === clickId) {
 
-$("#Дом_2").click(function () {
-    $("#Дом_2").hide();
-    $("#Дом_3").show();
+    //    $('.house.Yl2a > #Дом_1').hide();
+    //    $('.house.Yl2a > #Дом_2').show();
+        
+    //}
+
+    //else if ($("house.Yl2b".clickId)) {
+    //    $('.house.Yl2b > #Дом_1').hide();
+    //    $('.house.Yl2b > #Дом_2').show();
+        
+    //}
+
+    //else if ($("house.Yl2c".clickId)) {
+
+    //    $('.house.Yl2c + #Дом_1').hide();
+    //    $('.house.Yl2c + #Дом_2').show();
+        
+    //}
+
+
+    //else if (clickId = $(".house.Yl2d")) {
+
+    //    $('.house.Yl2d + #Дом_1').hide();
+    //    $('.house.Yl2d + #Дом_2').show();
+
+    //}
+    //else if (clickId = $(".house.Yl2e")) {
+
+    //    $('.house.Yl2e + #Дом_1').hide();
+    //    $('.house.Yl2e + #Дом_2').show();
+
+    //}
+
 
 });
 
-$("#Дом_3").click(function () {
-    $("#Дом_3").hide();
-    $("#Дом_4").show();
+$('.house.Yl2a > #Дом_2').click(function () {
+
+    console.log($(this).parent().attr("class"))
+
+    $('.house.Yl2a > #Дом_2').hide();
+    $('.house.Yl2a > #Дом_3').show();
 
 });
 
-$("#Дом_4").click(function () {
-    $("#Дом_4").hide();
-    $("#Дом_5").show();
+$('.house.Yl2a > #Дом_3').click(function () {
+
+    console.log($(this).parent().attr("class"))
+
+    $('.house.Yl2a > #Дом_3').hide();
+    $('.house.Yl2a > #Дом_4').show();
 
 });
+
+$('.house.Yl2a > #Дом_4').click(function () {
+
+
+    $('.house.Yl2a > #Дом_4').hide();
+    $('.house.Yl2a > #Дом_5').show();
+
+});
+
+
+// house.Yl2b
+$('.house.Yl2b > #Дом_1').click(function () {
+
+    $('.house.Yl2b > #Дом_1').hide();
+    $('.house.Yl2b > #Дом_2').show();
+});
+
+$('.house.Yl2b > #Дом_2').click(function () {
+
+    $('.house.Yl2b > #Дом_2').hide();
+    $('.house.Yl2b > #Дом_3').show();
+
+});
+
+$('.house.Yl2b > #Дом_3').click(function () {
+
+    $('.house.Yl2b > #Дом_3').hide();
+    $('.house.Yl2b > #Дом_4').show();
+
+});
+
+$('.house.Yl2b > #Дом_4').click(function () {
+
+    $('.house.Yl2b > #Дом_4').hide();
+    $('.house.Yl2b > #Дом_5').show();
+
+});
+
+// house.Yl2c
+$('.house.Yl2c > #Дом_1').click(function () {
+
+    $('.house.Yl2c > #Дом_1').hide();
+    $('.house.Yl2c > #Дом_2').show();
+});
+
+$('.house.Yl2c > #Дом_2').click(function () {
+
+    $('.house.Yl2c > #Дом_2').hide();
+    $('.house.Yl2c > #Дом_3').show();
+
+});
+
+$('.house.Yl2b > #Дом_3').click(function () {
+
+    $('.house.Yl2c > #Дом_3').hide();
+    $('.house.Yl2c > #Дом_4').show();
+
+});
+
+$('.house.Yl2c > #Дом_4').click(function () {
+
+    $('.house.Yl2c > #Дом_4').hide();
+    $('.house.Yl2c > #Дом_5').show();
+
+});
+
+
+// house.Yl2d
+$('.house.Yl2d > #Дом_1').click(function () {
+
+    $('.house.Yl2d > #Дом_1').hide();
+    $('.house.Yl2d > #Дом_2').show();
+});
+
+$('.house.Yl2d > #Дом_2').click(function () {
+
+    $('.house.Yl2d > #Дом_2').hide();
+    $('.house.Yl2d > #Дом_3').show();
+
+});
+
+$('.house.Yl2d > #Дом_3').click(function () {
+
+    $('.house.Yl2d > #Дом_3').hide();
+    $('.house.Yl2d > #Дом_4').show();
+
+});
+
+$('.house.Yl2d > #Дом_4').click(function () {
+
+    $('.house.Yl2d > #Дом_4').hide();
+    $('.house.Yl2d > #Дом_5').show();
+
+});
+
+// house.Yl2e
+$('.house.Yl2e > #Дом_1').click(function () {
+
+    $('.house.Yl2e > #Дом_1').hide();
+    $('.house.Yl2e > #Дом_2').show();
+});
+
+$('.house.Yl2e > #Дом_2').click(function () {
+
+    $('.house.Yl2e > #Дом_2').hide();
+    $('.house.Yl2e > #Дом_3').show();
+
+});
+
+$('.house.Yl2e > #Дом_3').click(function () {
+
+    $('.house.Yl2e > #Дом_3').hide();
+    $('.house.Yl2e > #Дом_4').show();
+
+});
+
+$('.house.Yl2e > #Дом_4').click(function () {
+
+    $('.house.Yl2e > #Дом_4').hide();
+    $('.house.Yl2e > #Дом_5').show();
+
+});
+
+
+// house.Yl1a
+$('.house.Yl1a > #Дом_1').click(function () {
+
+    $('.house.Yl1a > #Дом_1').hide();
+    $('.house.Yl1a > #Дом_2').show();
+});
+
+$('.house.Yl1a > #Дом_2').click(function () {
+
+    $('.house.Yl1a > #Дом_2').hide();
+    $('.house.Yl1a > #Дом_3').show();
+
+});
+
+$('.house.Yl1a > #Дом_3').click(function () {
+
+    $('.house.Yl1a > #Дом_3').hide();
+    $('.house.Yl1a > #Дом_4').show();
+
+});
+
+$('.house.Yl1a > #Дом_4').click(function () {
+
+    $('.house.Yl1a > #Дом_4').hide();
+    $('.house.Yl1a > #Дом_5').show();
+
+});
+
+
+// house.Yl1b
+$('.house.Yl1b > #Дом_1').click(function () {
+
+    $('.house.Yl1b > #Дом_1').hide();
+    $('.house.Yl1b > #Дом_2').show();
+});
+
+$('.house.Yl1b > #Дом_2').click(function () {
+
+    $('.house.Yl1b > #Дом_2').hide();
+    $('.house.Yl1b > #Дом_3').show();
+
+});
+
+$('.house.Yl1b > #Дом_3').click(function () {
+
+    $('.house.Yl1b > #Дом_3').hide();
+    $('.house.Yl1b > #Дом_4').show();
+
+});
+
+$('.house.Yl1b > #Дом_4').click(function () {
+
+    $('.house.Yl1b > #Дом_4').hide();
+    $('.house.Yl1b > #Дом_5').show();
+
+});
+
 
