7fd76a481902472184708dbc1f8e49d1256e9771
Antonina <polonchuk.a@gmail.com>
2022-08-12 08:24:30 +0300

    <Добавила кнопки отменить-улучшить и рост домиков через кнопки >

--- JavaScript1.js
+++ JavaScript1.js
@@ -3,39 +3,103 @@
 /*console.log($(this).parent().attr("class"))*/
 
 
+
 $('#Дом_1').click(function () {
 
-    let getID = $(this).parent().attr("id")
+    let getID = $(this).parent().attr("id");
+    let check = false;
 
-    $('#' + getID + ' #Дом_1').hide();
-    $('#' + getID + ' #Дом_2').show();
+    $('.buttons').css({ 'bottom': '-35px' }); 
+    check = true;
 
+    $('#Отмена').click(function () {
+
+        $('.buttons').css({ 'bottom': '-150px' });
+        check = false;
+    });
+
+    $('#Улучшить').click(function () {
+        if (check == true) {
+            $('#' + getID + ' #Дом_1').hide();
+            $('#' + getID + ' #Дом_2').show();
+            $('.buttons').css({ 'bottom': '-150px' });
+            check = false;
+        }
+    });
 });
 
 $('#Дом_2').click(function () {
 
-    let getID = $(this).parent().attr("id")
+    let getID = $(this).parent().attr("id");
+    let check = false;
 
-    $('#' + getID + ' #Дом_2').hide();
-    $('#' + getID + ' #Дом_3').show();
+    $('.buttons').css({ 'bottom': '-35px' });
+    check = true;
+
+    $('#Отмена').click(function () {
+
+        $('.buttons').css({ 'bottom': '-150px' });
+        check = false;
+    });
+
+    $('#Улучшить').click(function () {
+        if (check == true) {
+            $('#' + getID + ' #Дом_2').hide();
+            $('#' + getID + ' #Дом_3').show();
+            $('.buttons').css({ 'bottom': '-150px' });
+            check = false;
+        }
+    });
 
 });
 
 $('#Дом_3').click(function () {
 
-    let getID = $(this).parent().attr("id")
+    let getID = $(this).parent().attr("id");
+    let check = false;
 
-    $('#' + getID + ' #Дом_3').hide();
-    $('#' + getID + ' #Дом_4').show();
+    $('.buttons').css({ 'bottom': '-35px' });
+    check = true;
+
+    $('#Отмена').click(function () {
+
+        $('.buttons').css({ 'bottom': '-150px' });
+        check = false;
+    });
+
+    $('#Улучшить').click(function () {
+        if (check == true) {
+            $('#' + getID + ' #Дом_3').hide();
+            $('#' + getID + ' #Дом_4').show();
+            $('.buttons').css({ 'bottom': '-150px' });
+            check = false;
+        }
+    });
 
 });
 
 $('#Дом_4').click(function () {
 
-    let getID = $(this).parent().attr("id")
+    let getID = $(this).parent().attr("id");
+    let check = false;
 
-    $('#' + getID + ' #Дом_4').hide();
-    $('#' + getID + ' #Дом_5').show();
+    $('.buttons').css({ 'bottom': '-35px' });
+    check = true;
+
+    $('#Отмена').click(function () {
+
+        $('.buttons').css({ 'bottom': '-150px' });
+        check = false;
+    });
+
+    $('#Улучшить').click(function () {
+        if (check == true) {
+            $('#' + getID + ' #Дом_4').hide();
+            $('#' + getID + ' #Дом_5').show();
+            $('.buttons').css({ 'bottom': '-150px' });
+            check = false;
+        }
+    });
 
 });
 
