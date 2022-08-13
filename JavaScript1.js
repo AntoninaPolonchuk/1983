1f7fb86b4976403a46084353f7a4d545ae2990b4
Antonina <polonchuk.a@gmail.com>
2022-08-13 13:59:57 +0300

    <добавила возможность строить домики с нуля>

--- JavaScript1.js
+++ JavaScript1.js
@@ -2,6 +2,51 @@
 
 /*console.log($(this).parent().attr("class"))*/
 
+/*Домики, которые строятся с поля*/
+
+$('#Поле').click(function () {
+
+    let getID = $(this).parent().attr("id");
+    /*проверка для того, чтобы не строились все домики, на которые был клик*/
+    let check = false;
+    let check2 = false;
+
+    $('.buttons2').css({ 'bottom': '50px' });
+    check = true;
+    forbidClick();
+
+    /*прозрачный домик до выбора*/
+    check2 = true;
+    $('#' + getID + ' #Поле').hide();
+    $('#' + getID + ' #Дом_1').show().css({ 'fill-opacity': '0.5' });
+
+
+    $('#Отмена2').click(function () {
+
+        $('.buttons2').css({ 'bottom': '-150px' });
+
+        /*убираем прозначный домик*/
+        if (check2 == true & check == true) {
+            $('#' + getID + ' #Поле').show();
+            $('#' + getID + ' #Дом_1').hide();
+            check2 == false;
+        }
+        check = false;
+        allowClick();
+    });
+
+    $('#Построить').click(function () {
+        if (check == true) {
+            $('#' + getID + ' #Поле').hide();
+            $('#' + getID + ' #Дом_1').show().css({ 'fill-opacity': '1' });
+            $('.buttons2').css({ 'bottom': '-150px' });
+            check = false;
+            allowClick();
+            check2 = false;
+        }
+    });
+});
+
 
 $('#Дом_1').click(function () {
 
@@ -10,11 +55,14 @@ $('#Дом_1').click(function () {
 
     $('.buttons').css({ 'bottom': '50px' }); 
     check = true;
+    forbidClick();
+
 
     $('#Отмена').click(function () {
 
         $('.buttons').css({ 'bottom': '-150px' });
         check = false;
+        allowClick();
     });
 
     $('#Улучшить').click(function () {
@@ -23,9 +71,39 @@ $('#Дом_1').click(function () {
             $('#' + getID + ' #Дом_2').show();
             $('.buttons').css({ 'bottom': '-150px' });
             check = false;
+            allowClick();
         }
     });
 });
+
+/*Домики, которые со старта*/
+$('#Дом_1a').click(function () {
+
+    let getID = $(this).parent().attr("id");
+    let check = false;
+
+    $('.buttons').css({ 'bottom': '50px' });
+    check = true;
+    forbidClick();
+
+    $('#Отмена').click(function () {
+
+        $('.buttons').css({ 'bottom': '-150px' });
+        check = false;
+        allowClick();
+    });
+
+    $('#Улучшить').click(function () {
+        if (check == true) {
+            $('#' + getID + ' #Дом_1').hide();
+            $('#' + getID + ' #Дом_2').show();
+            $('.buttons').css({ 'bottom': '-150px' });
+            check = false;
+            allowClick();
+        }
+    });
+});
+
 
 $('#Дом_2').click(function () {
 
@@ -34,11 +112,13 @@ $('#Дом_2').click(function () {
 
     $('.buttons').css({ 'bottom': '50px' });
     check = true;
+    forbidClick();
 
     $('#Отмена').click(function () {
 
         $('.buttons').css({ 'bottom': '-150px' });
         check = false;
+        allowClick();
     });
 
     $('#Улучшить').click(function () {
@@ -47,6 +127,7 @@ $('#Дом_2').click(function () {
             $('#' + getID + ' #Дом_3').show();
             $('.buttons').css({ 'bottom': '-150px' });
             check = false;
+            allowClick();
         }
     });
 
@@ -59,11 +140,13 @@ $('#Дом_3').click(function () {
 
     $('.buttons').css({ 'bottom': '50px' });
     check = true;
+    forbidClick();
 
     $('#Отмена').click(function () {
 
         $('.buttons').css({ 'bottom': '-150px' });
         check = false;
+        allowClick();
     });
 
     $('#Улучшить').click(function () {
@@ -72,6 +155,7 @@ $('#Дом_3').click(function () {
             $('#' + getID + ' #Дом_4').show();
             $('.buttons').css({ 'bottom': '-150px' });
             check = false;
+            allowClick();
         }
     });
 
@@ -84,11 +168,13 @@ $('#Дом_4').click(function () {
 
     $('.buttons').css({ 'bottom': '50px' });
     check = true;
+    forbidClick();
 
     $('#Отмена').click(function () {
 
         $('.buttons').css({ 'bottom': '-150px' });
         check = false;
+        allowClick();
     });
 
     $('#Улучшить').click(function () {
@@ -97,10 +183,29 @@ $('#Дом_4').click(function () {
             $('#' + getID + ' #Дом_5').show();
             $('.buttons').css({ 'bottom': '-150px' });
             check = false;
+            allowClick();
         }
     });
-
 });
 
 
+/*Убираем возможность повторного клика на домик*/
+function forbidClick() {
+    $('#Поле').css({ 'pointer-events': 'none' })
+    $('#Дом_1a').css({ 'pointer-events': 'none' })
+    $('#Дом_1').css({ 'pointer-events': 'none' })
+    $('#Дом_2').css({ 'pointer-events': 'none' })
+    $('#Дом_3').css({ 'pointer-events': 'none' })
+    $('#Дом_4').css({ 'pointer-events': 'none' })
+    $('#Дом_5').css({ 'pointer-events': 'none' })
+}
 
+function allowClick() {
+    $('#Поле').css({ 'pointer-events': 'auto' })
+    $('#Дом_1a').css({ 'pointer-events': 'auto' })
+    $('#Дом_1').css({ 'pointer-events': 'auto' })
+    $('#Дом_2').css({ 'pointer-events': 'auto' })
+    $('#Дом_3').css({ 'pointer-events': 'auto' })
+    $('#Дом_4').css({ 'pointer-events': 'auto' })
+    $('#Дом_5').css({ 'pointer-events': 'auto' })
+}
